66d476cf6a0af686b0b5fdc7eaba8ae8548bac18
dd-henry <henry.azevedo06@gmail.com>
2025-09-20 02:10:00 -0300

    feat: map environment variables for Dokploy in Docker Compose

--- src/main/java/com/henry/dev/email_service/scheduler/ProduceMailScheduler.java
+++ src/main/java/com/henry/dev/email_service/scheduler/ProduceMailScheduler.java
@@ -21,7 +21,7 @@ public class ProduceMailScheduler {
     private final ScheduledEmailsRepositoryPort repository;
     private final EmailProducer producer;
 
-    @Scheduled(cron = "0 0 1,6,9,12,15,18 * * *")
+    @Scheduled(cron = "0 15 1,6,9,12,15,18 * * *")
 //    @Scheduled(cron = "0 */1 * * * *")
     public void execute() {
         log.info("Iniciando verificação de emails agendados...");
